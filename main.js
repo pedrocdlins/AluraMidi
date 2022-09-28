68ee4498b7a91f7e27f0883a5cd5cbd25491fa1d
pedrocdlins <pedrocdlins@gmail.com>
2022-09-28 10:18:36 -0300

    Terminando projeto JS para o AluraMidi

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        alert('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -14,8 +21,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom (idAudio);
     }
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove ('ativa');
     }
 
 }
